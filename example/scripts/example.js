d9a45c3eb04ca6971936c1eb946f88d4f0cd5475
Yaw Joseph Etse <yaw.etse@gmail.com>
2013-12-01 22:31:48 -0500

    header template

--- example/scripts/example.js
+++ example/scripts/example.js
@@ -2704,27 +2704,29 @@ var module1 = webapp,
 	fullWidthSlideshow2 = false;
 
 window.onload = function(){
-	// async.parallel({
-	//     template: function(callback){
-	// 		module1.grabTemplate(window.document.getElementById('component-template').innerHTML,callback);
-	//     },
-	//     componentData: function(callback){
- //            module1.grabData('https://s3.amazonaws.com/gpsampledata/component.list-view-scroll/contentspec.json',callback);
-	//     }
-	// },
-	// function(err, results) {
-	// 	if(err){
-	// 		console.log(err);
-	// 	}
-	// 	webapp.render( results.template, results.componentData, "slider1");
-	// 	fullWidthSlideshow1 = new fullWidthSlideshow({element:"p_c_lvs-id"});
+	async.parallel({
+	    template: function(callback){
+			module1.grabTemplate(window.document.getElementById('navigation-template').innerHTML,callback);
+	    },
+	    componentData: function(callback){
+            module1.grabData('https://s3.amazonaws.com/gpsampledata/component.navigation-header/navigationspec.json',callback);
+	    }
+	},
+	function(err, results) {
+		if(err){
+			console.log(err);
+		}
 
-	// 	var data2 = results.componentData;
-	// 	data2.config.html.dom_id="p_c_lvs-id2";
-	// 	data2.config.html.preview=true;
-	// 	webapp.render( results.template, data2, "slider2");
-	// 	fullWidthSlideshow2 = new fullWidthSlideshow({element:"p_c_lvs-id2"});
-	// });
+		console.log("results",results);
+		webapp.render( results.template, results.componentData, "header-container");
+		// fullWidthSlideshow1 = new fullWidthSlideshow({element:"p_c_lvs-id"});
+
+		// var data2 = results.componentData;
+		// data2.config.html.dom_id="p_c_lvs-id2";
+		// data2.config.html.preview=true;
+		// webapp.render( results.template, data2, "slider2");
+		// fullWidthSlideshow2 = new fullWidthSlideshow({element:"p_c_lvs-id2"});
+	});
 };
 
 // module1.on("grabbedData",function(){
